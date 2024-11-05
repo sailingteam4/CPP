f10007f2ff34546cc17139c7ea8c8e8dd698d48a
sailingteam4 <sailing4team@gmail.com>
2024-11-05 13:28:54 +0100

    rework for ex01

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -12,21 +12,24 @@
 
 #include <iostream>
 
+int to_upper(int ac, char **av)
+{
+    for (int i = 1; i < ac; i++)
+    {
+        for (int j = 0; av[i][j]; j++)
+            if (av[i][j] >= 'a' && av[i][j] <= 'z')
+                std::cout << (char)toupper(av[i][j]);
+            else
+                std::cout << (char)av[i][j];
+    }
+    std::cout << std::endl;
+    return (0);
+}
+
 int	main(int ac, char **av)
 {
-	if (ac == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < ac; i++)
-		{
-			for (int j = 0; av[i][j]; j++)
-				if (av[i][j] >= 'a' && av[i][j] <= 'z')
-					std::cout << (char)toupper(av[i][j]);
-				else
-					std::cout << (char)av[i][j];
-		}
-		std::cout << std::endl;
-	}
+	if (ac != 1)
+	    return (to_upper(ac, av));
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return (0);
 }

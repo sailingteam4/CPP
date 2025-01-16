b38deb5de399742895ef949a73069a1c17a8cd64
sailingteam4 <sailing4team@gmail.com>
2025-01-16 14:29:14 +0100

    add colors

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,60 +6,70 @@
 /*   By: nrontey <nrontey@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 00:04:32 by nrontey           #+#    #+#             */
-/*   Updated: 2025/01/15 16:02:51 by nrontey          ###   ########.fr       */
+/*   Updated: 2025/01/16 14:20:03 by nrontey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include "WrongCat.hpp"
+#include <iostream>
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
 
 int main()
 {
-    std::cout << "----- Constructor tests -----" << std::endl << std::endl;
+	std::cout << YELLOW << "----- Constructor tests -----" << RESET << std::endl << std::endl;
 
-    std::cout << "Creating Animal objects: " << std::endl << std::endl;
-    const Animal* meta = new Animal();
-    std::cout << std::endl << "Creating Dog objects: " << std::endl << std::endl;
-    const Animal* j = new Dog();
-    std::cout << std::endl << "Creating Cat objects: " << std::endl << std::endl;
-    const Animal* i = new Cat();
+	std::cout << GREEN << "Creating Animal objects: " << RESET << std::endl << std::endl;
+	const Animal* meta = new Animal();
+	std::cout << std::endl << GREEN << "Creating Dog objects: " << RESET << std::endl << std::endl;
+	const Animal* j = new Dog();
+	std::cout << std::endl << GREEN << "Creating Cat objects: " << RESET << std::endl << std::endl;
+	const Animal* i = new Cat();
 
-    std::cout << std::endl << "----- Sound tests -----" << std::endl << std::endl;
+	std::cout << std::endl << YELLOW << "----- Sound tests -----" << RESET << std::endl << std::endl;
 
-    std::cout << "Calling makeSound() on Cat objects: " << std::endl << std::endl;
-    i->makeSound();
-    std::cout << "Calling makeSound() on Dog objects: " << std::endl << std::endl;
-    j->makeSound();
-    std::cout << "Calling makeSound() on Animal objects: " << std::endl << std::endl;
-    meta->makeSound();
+	std::cout << BLUE << "Calling makeSound() on Cat objects: " << RESET << std::endl << std::endl;
+	i->makeSound();
+	std::cout << BLUE << "Calling makeSound() on Dog objects: " << RESET << std::endl << std::endl;
+	j->makeSound();
+	std::cout << BLUE << "Calling makeSound() on Animal objects: " << RESET << std::endl << std::endl;
+	meta->makeSound();
 
-    std::cout << std::endl << "----- Constructors tests -----" << std::endl << std::endl;
+	std::cout << std::endl << YELLOW << "----- Constructors tests -----" << RESET << std::endl << std::endl;
 
-    std::cout << "Creating WrongAnimal object: " << std::endl << std::endl;
-    const WrongAnimal* wrong_meta = new WrongAnimal();
-    std::cout << std::endl << "Creating WrongCat object: " << std::endl << std::endl;
-    const WrongAnimal* wrong_j = new WrongCat();
+	std::cout << GREEN << "Creating WrongAnimal object: " << RESET << std::endl << std::endl;
+	const WrongAnimal* wrong_meta = new WrongAnimal();
+	std::cout << std::endl << GREEN << "Creating WrongCat object: " << RESET << std::endl << std::endl;
+	const WrongAnimal* wrong_j = new WrongCat();
 
-    std::cout << std::endl << "----- Sound tests -----" << std::endl << std::endl;
+	std::cout << std::endl << YELLOW << "----- Sound tests -----" << RESET << std::endl << std::endl;
 
-    std::cout << "Calling makeSound() on WrongCat object: " << std::endl << std::endl;
-    wrong_j->makeSound();
-    std::cout << "Calling makeSound() on WrongAnimal object: " << std::endl << std::endl;
-    wrong_meta->makeSound();
+	std::cout << BLUE << "Calling makeSound() on WrongCat object: " << RESET << std::endl << std::endl;
+	wrong_j->makeSound();
+	std::cout << BLUE << "Calling makeSound() on WrongAnimal object: " << RESET << std::endl << std::endl;
+	wrong_meta->makeSound();
 
-    std::cout << std::endl << "----- Destructor tests -----" << std::endl << std::endl;
+	std::cout << std::endl << YELLOW << "----- Destructor tests -----" << RESET << std::endl << std::endl;
 
-    std::cout << "Deleting Animal object: " << std::endl << std::endl;
-    delete meta;
-    std::cout << "Deleting Dog object: " << std::endl << std::endl;
-    delete j;
-    std::cout << "Deleting Cat object: " << std::endl << std::endl;
-    delete i;
-    std::cout << "Deleting WrongAnimal object: " << std::endl << std::endl;
-    delete wrong_meta;
-    std::cout << "Deleting WrongCat object: " << std::endl << std::endl;
-    delete wrong_j;
+	std::cout << RED << "Deleting Animal object: " << RESET << std::endl << std::endl;
+	delete meta;
+	std::cout << RED << "Deleting Dog object: " << RESET << std::endl << std::endl;
+	delete j;
+	std::cout << RED << "Deleting Cat object: " << RESET << std::endl << std::endl;
+	delete i;
+	std::cout << RED << "Deleting WrongAnimal object: " << RESET << std::endl << std::endl;
+	delete wrong_meta;
+	std::cout << RED << "Deleting WrongCat object: " << RESET << std::endl << std::endl;
+	delete wrong_j;
 
-    return 0;
+	return 0;
 }
--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,84 +6,93 @@
 /*   By: nrontey <nrontey@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 00:04:32 by nrontey           #+#    #+#             */
-/*   Updated: 2025/01/16 00:44:53 by nrontey          ###   ########.fr       */
+/*   Updated: 2025/01/16 14:28:11 by nrontey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
 int main()
 {
-    std::cout << "----- Test AAnimal can't be instantiated -----" << std::endl << std::endl;
+	std::cout << YELLOW << "----- Test AAnimal can't be instantiated -----" << RESET << std::endl << std::endl;
 
-    //AAnimal test; // This line will not compile
+	//AAnimal test; // This line will not compile
 
-    std::cout << "----- Basic constructor tests -----" << std::endl << std::endl;
+	std::cout << GREEN << "----- Basic constructor tests -----" << RESET << std::endl << std::endl;
 
-    const AAnimal* j = new Dog();
-    const AAnimal* i = new Cat();
+	const AAnimal* j = new Dog();
+	const AAnimal* i = new Cat();
 
-    std::cout << std::endl << "----- Basic destructor tests -----" << std::endl << std::endl;
+	std::cout << std::endl << RED << "----- Basic destructor tests -----" << RESET << std::endl << std::endl;
 
-    delete j;
-    delete i;
+	delete j;
+	delete i;
 
-    std::cout << std::endl << "----- Arrays tests -----" << std::endl << std::endl;
+	std::cout << std::endl << BLUE << "----- Arrays tests -----" << RESET << std::endl << std::endl;
 
-    const int n = 4;
-    std::cout << "Creating an array of " << n << " animals" << std::endl;
-    AAnimal* animals[n];
+	const int n = 4;
+	std::cout << "Creating an array of " << n << " animals" << std::endl;
+	AAnimal* animals[n];
 
-    std::cout << std::endl << "Creating " << n / 2 << " dogs" << std::endl << std::endl;
+	std::cout << std::endl << "Creating " << n / 2 << " dogs" << std::endl << std::endl;
 
-    for (int i = 0; i < n / 2; i++)
-    {
-        animals[i] = new Dog();
-        std::cout << std::endl;
-    }
+	for (int i = 0; i < n / 2; i++)
+	{
+		animals[i] = new Dog();
+		std::cout << std::endl;
+	}
 
-    std::cout << std::endl << "Creating " << n / 2 << " cats" << std::endl << std::endl;
+	std::cout << std::endl << "Creating " << n / 2 << " cats" << std::endl << std::endl;
 
-    for (int i = n / 2; i < n; i++)
-    {
-        animals[i] = new Cat();
-        std::cout << std::endl;
-    }
+	for (int i = n / 2; i < n; i++)
+	{
+		animals[i] = new Cat();
+		std::cout << std::endl;
+	}
 
-    std::cout << std::endl << "Deleting all animals" << std::endl << std::endl;
+	std::cout << std::endl << MAGENTA << "Deleting all animals" << RESET << std::endl << std::endl;
 
-    for (int i = 0; i < n; i++)
-    {
-        delete animals[i];
-        std::cout << std::endl;
-    }
+	for (int i = 0; i < n; i++)
+	{
+		delete animals[i];
+		std::cout << std::endl;
+	}
 
-    std::cout << std::endl << "----- Copy constructor tests -----" << std::endl << std::endl;
+	std::cout << std::endl << CYAN << "----- Copy constructor tests -----" << RESET << std::endl << std::endl;
 
-    Dog dog;
-    Dog dog2(dog);
+	Dog dog;
+	Dog dog2(dog);
 
-    std::cout << std::endl;
+	std::cout << std::endl;
 
-    Cat cat;
-    Cat cat2(cat);
+	Cat cat;
+	Cat cat2(cat);
 
-    std::cout << std::endl << "----- Assignation operator tests -----" << std::endl << std::endl;
+	std::cout << std::endl << WHITE << "----- Assignation operator tests -----" << RESET << std::endl << std::endl;
 
-    Dog dog3;
-    Dog dog4;
+	Dog dog3;
+	Dog dog4;
 
-    dog4 = dog3;
+	dog4 = dog3;
 
-    std::cout << std::endl;
+	std::cout << std::endl;
 
-    Cat cat3;
-    Cat cat4;
+	Cat cat3;
+	Cat cat4;
 
-    cat4 = cat3;
+	cat4 = cat3;
 
-    std::cout << "----- End of tests destructors here -----" << std::endl;
+	std::cout << YELLOW << "----- End of tests destructors here -----" << RESET << std::endl;
 
-    return 0;
+	return 0;
 }

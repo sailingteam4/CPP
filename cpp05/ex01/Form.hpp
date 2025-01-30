e425dd58235ae711347d0d1016c4614f582e0398
sailingteam4 <sailing4team@gmail.com>
2025-01-30 11:51:50 +0100

    cpp05ex01

---
+++ cpp05/ex01/Form.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrontey <nrontey@student.42angouleme.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 11:29:32 by nrontey           #+#    #+#             */
+/*   Updated: 2025/01/30 11:51:31 by nrontey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FORM_HPP
+# define FORM_HPP
+
+#include <iostream>
+#include <string>
+
+class Form
+{
+	private:
+		const std::string	_name;
+		bool				_signed;
+		const int			_gradeToSign;
+		const int			_gradeToExecute;
+
+	public:
+		Form(std::string name, int gradeToSign, int gradeToExecute);
+		Form();
+		Form(Form const & src);
+		~Form();
+
+		Form & operator=(Form const & src);
+
+		std::string	getName() const;
+		bool		getSigned() const;
+		int			getGradeToSign() const;
+		int			getGradeToExecute() const;
+
+		void		beSigned(Bureaucrat & bureaucrat);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+}
+
+std::ostream & operator<<(std::ostream & os, Form const & src);
+
+#endif

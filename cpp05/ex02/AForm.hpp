bf47747684ebe6bba7afa587b326c7b83f782396
sailingteam4 <sailing4team@gmail.com>
2025-02-11 01:00:06 +0100

    cpp05ex02

--- cpp05/ex02/AForm.hpp
+++ cpp05/ex02/AForm.hpp
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AForm.hpp                                           :+:      :+:    :+:   */
+/*   AAForm.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nrontey <nrontey@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 11:29:32 by nrontey           #+#    #+#             */
-/*   Updated: 2025/01/30 13:14:24 by nrontey          ###   ########.fr       */
+/*   Updated: 2025/02/04 16:58:26 by nrontey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef AForm_HPP
-# define AForm_HPP
+#ifndef AFORM_HPP
+# define AFORM_HPP
 
 #include <iostream>
 #include <string>
@@ -20,7 +20,7 @@ class Bureaucrat;
 
 class AForm
 {
-	private:
+	protected:
 		const std::string	_name;
 		bool				_signed;
 		const int			_gradeToSign;
